7dc60362aac84f512bd5e656ce73054b4259deba
emmyjaff <emmyjaff22@gmail.com>
2022-07-23 01:26:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './hompage-component/header';
+import Futured from './hompage-component/futured';
+import Cta from './hompage-component/cta';
+import Navbar from './hompage-component/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Navbar /> */}
+      <Header />
+      <Futured />
+      <Cta />
     </div>
   );
 }
